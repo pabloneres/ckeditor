cd626de174eadbb4e2fa3d325617af0ebbcaea13
Pablo Neres <pabloneres@hotmail.com>
2023-06-21 16:56:36 -0300

    update

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -3,67 +3,69 @@
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
-import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js';
+import ImageBlock from '@ckeditor/ckeditor5-image/src/imageblock.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
 import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
 import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert.js';
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
-import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js';
+import Style from '@ckeditor/ckeditor5-style/src/style.js';
 import Table from '@ckeditor/ckeditor5-table/src/table.js';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
-	AutoImage,
+	Alignment,
 	Autoformat,
-	Base64UploadAdapter,
+	ImageBlock,
 	BlockQuote,
 	Bold,
 	CloudServices,
+	Code,
 	Essentials,
+	FontColor,
 	FontSize,
+	GeneralHtmlSupport,
 	Heading,
-	Image,
+	HorizontalLine,
 	ImageInsert,
-	ImageResize,
 	ImageStyle,
-	ImageToolbar,
 	ImageUpload,
 	Indent,
-	IndentBlock,
 	Italic,
 	Link,
 	List,
-	Markdown,
-	MediaEmbed,
 	Mention,
 	Paragraph,
 	PasteFromOffice,
+	SimpleUploadAdapter,
+	Style,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	Underline
 ];
 
 // Editor configuration.
@@ -72,31 +74,31 @@ Editor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
-			'fontSize',
 			'bold',
 			'italic',
+			'underline',
+			'link',
 			'bulletedList',
 			'numberedList',
 			'|',
 			'outdent',
 			'indent',
 			'|',
+			'imageUpload',
 			'blockQuote',
 			'insertTable',
 			'undo',
 			'redo',
-			'imageUpload'
+			'imageInsert',
+			'style',
+			'alignment',
+			'code',
+			'fontSize',
+			'horizontalLine',
+			'fontColor'
 		]
 	},
 	language: 'pt-br',
-	image: {
-		toolbar: [
-			'imageTextAlternative',
-			'imageStyle:inline',
-			'imageStyle:block',
-			'imageStyle:side'
-		]
-	},
 	table: {
 		contentToolbar: [
 			'tableColumn',
